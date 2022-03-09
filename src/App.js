18a2bc5cd21977d8aee5b68cdbc29ec738ea7dee
Quyen Pham <hoangquyen174@gmail.com>
2022-03-08 19:37:09 -0600

    add form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Menu from './Menu';
 import NavBar from './NavBar';
+import AddForm from './AddForm';
+import slugify from 'slugify';
+import Item from "./Item";
 import { fetchItems, addItem as addItemApi} from './Api';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
-  const [menu, setMenu] =useState({
+  const [menu, setMenu] = useState({
     snacks: [],
     drinks: []
   });
+
+  // load data from backend
 
   useEffect(() => {
     async function getAllItems() {
@@ -24,26 +29,60 @@ function App() {
     getAllItems();
   }, []);
 
+  async function addItem(type, {name, description, recipe, serve}) {
+    let id = slugify(name, {lower: true});
+    let objData = { id, name, description, recipe, serve };
+    await addItemApi(type, objData);
+    setMenu(m => ({
+      ...m,
+      [type]: [...m[type], objData]
+    }));
+  }
+
   let { snacks, drinks } = menu;
+
+  if(!isLoading) {
+    return <p>Loading &hellip;</p>
+  }
 
   return (
     <div className="App">
       <BrowserRouter>
       <NavBar />
+      <main>
       <Switch>
         <Route exact path="/">
           <Home snacks={snacks} drinks={drinks} />
         </Route>
 
         <Route exact path="/snacks">
-          <Menu items={drinks} title="Snacks" />
+          <Menu items={snacks} title="Snacks" />
         </Route>
 
         <Route exact path="/drinks">
           <Menu items={drinks} title="Drinks" />
         </Route>
 
+        <Route path="/snacks/:id">
+          <Item items={snacks} cantFind="/snacks" />
+        </Route>
+
+        <Route path="/drinks/:id">
+          <Item items={drinks} cantFind="/drinks"  />
+        </Route>
+
+        <Route path="/add">
+          <AddForm addItem={addItem} />
+        </Route>
+
+        <Route>
+          <p>Hmmm. I can't seem to find what you want.</p>
+        </Route>
+
+
       </Switch>
+      </main>
+     
       </BrowserRouter>
     
     </div>
